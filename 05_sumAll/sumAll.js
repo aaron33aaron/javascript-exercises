3393c6bafffa2218e2cde5bface2f4f6e07a8299
aaron33aaron <aaron33aaron@outlook.com>
2022-12-15 00:13:55 -0500

    Finished more exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    let sum = 0;
+    if (!Number.isInteger(firstNum) || !Number.isInteger(secondNum)) return "ERROR";
+    if (firstNum < 0 || secondNum < 0) {return "ERROR"}
 
+    else if (firstNum > secondNum) {
+        const temp = firstNum;
+        firstNum = secondNum;
+        secondNum = temp;
+    }
+
+        while (firstNum < secondNum)
+        {
+            firstNum++;
+
+            sum += firstNum;
+        }
+
+        return sum + 1;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    
+    const processedString = string.toLowerCase().replace(/[^a-z]/g, "");
+    return (processedString.split("").reverse().join("") == processedString);
 };
+
+palindromes("Hello");
 
 // Do not edit below this line
 module.exports = palindromes;
